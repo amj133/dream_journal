6530a647770f6ddb9b40ecc7ff4c0d0673ef7398
amj133 <amj@vt.edu>
2018-02-10 14:53:53 -0700

    user can delete dream feature test passing

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -25,11 +25,19 @@ class DreamsController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:user_id])
+    # @user = User.find(params[:user_id])
     @dream = Dream.find(params[:id])
     @dream.update(dream_params)
 
     redirect_to dream_path(@dream)
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @dream = Dream.find(params[:id])
+    @dream.destroy
+
+    redirect_to user_path(@user)
   end
 
   private
---
+++ spec/features/user_can_delete_dream_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "user can delete a dream" do
+  describe "user visits user page" do
+    it "has link to delete dream" do
+      bob = User.create!(user_name: "BobRocks15",
+                         email: "bobrocks@gmail.com")
+      dream = bob.dreams.create!(title: "Escape From Jungle",
+                                 body: "I was a worker on a jungle fortress, there was an overlord/slave driver with a whip, I escaped and flew away",
+                                 analysis: "I love action/adventure and the feeling of flying")
+
+      visit user_path(bob)
+      click_on('Delete')
+
+      expect(current_path).to eq(user_path(bob))
+      expect(bob.dreams.count).to eq(0)
+      expect(page).to_not have_content("Escape From Jungle")
+    end
+  end
+end
