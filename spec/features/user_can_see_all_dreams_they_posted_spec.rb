ef686d9198947586642d71b4df2468606298852c
amj133 <amj@vt.edu>
2018-02-10 11:29:22 -0700

    wrote user can see all dreams they have posted feature test

--- spec/features/user_can_see_all_dreams_they_posted_spec.rb
+++ spec/features/user_can_see_all_dreams_they_posted_spec.rb
@@ -5,7 +5,16 @@ describe "user sees all dreams they posted" do
     it "displays all dreams they have posted" do
       bob = User.create!(user_name: "BobRocks15",
                          email: "bobrocks@gmail.com")
-      bob.dreams.create!()
+      dream = bob.dreams.create!(title: "Escape From Jungle",
+                                 body: "I was a worker on a jungle fortress, there was an overlord/slave driver with a whip, I escaped and flew away",
+                                 analysis: "I love action/adventure and the feeling of flying")
+
+      visit user_path(bob)
+
+      expect(page).to have_content("BobRocks15's Dream Journal")
+      expect(page).to have_content(dream.title)
+      expect(page).to have_content(dream.body)
+      expect(page).to have_content(dream.analysis)
     end
   end
 end
