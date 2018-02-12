b85cfb935253ad15fde965721861edc800a8fe74
amj133 <amj@vt.edu>
2018-02-11 20:46:00 -0700

    basic styling on category index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "categories#index"
+
   resources :users, only: [:show] do
     resources :dreams, only: [:new, :create, :edit, :update, :destroy]
   end
@@ -12,4 +14,6 @@ Rails.application.routes.draw do
 
   resources :categories, only: [:index, :show]
 
+
 end
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
