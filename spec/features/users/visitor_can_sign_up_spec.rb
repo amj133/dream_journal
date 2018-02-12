22c5ef908c50184881b59a0ed12a6782cf3e67c9
amj133 <amj@vt.edu>
2018-02-12 13:29:22 -0700

    wrote visitor can sign up spec

---
+++ spec/features/users/visitor_can_sign_up_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "visitor can sign up as user" do
+  describe "from root can click sign up" do
+    it "creates a new user when they submit info" do
+      visit '/'
+      click_on('Sign Up')
+
+      expect(current_path).to eq(new_user_path)
+
+      fill_in('user[user_name]', with: "bobrocks13")
+      fill_in('user[email]', with: "brocks13@gmail.com")
+      fill_in('user[password]', with: "test")
+      click_on('Sign Up Now')
+
+      expect(current_path).to eq(user_path(User.last))
+      expect(page).to have_content("Welcome #{User.last.user_name}")
+    end
+  end
+end
