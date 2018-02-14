f422164a94e69d03e9aae0e22b8667722c3c81f9
amj133 <amj@vt.edu>
2018-02-14 11:10:38 -0700

    create AddTimestampsToComments migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180214175855) do
+ActiveRecord::Schema.define(version: 20180214180952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20180214175855) do
     t.string "user_name"
     t.text "body"
     t.bigint "dream_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["dream_id"], name: "index_comments_on_dream_id"
   end
 
