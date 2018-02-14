cf911a5309a4defd63a26831e2374f816ef7e8e9
amj133 <amj@vt.edu>
2018-02-14 06:53:41 -0700

    add timestamps to dreams and comments

--- db/migrate/20180210175958_create_dreams.rb
+++ db/migrate/20180210175958_create_dreams.rb
@@ -4,6 +4,7 @@ class CreateDreams < ActiveRecord::Migration[5.1]
       t.text :body
       t.text :analysis
       t.references :user, foreign_key: true
+      t.timestamps
     end
   end
 end
--- db/migrate/20180210221206_create_comments.rb
+++ db/migrate/20180210221206_create_comments.rb
@@ -4,6 +4,7 @@ class CreateComments < ActiveRecord::Migration[5.1]
       t.string :user_name
       t.text :body
       t.references :dream, foreign_key: true
+      t.timestamps
     end
   end
 end
